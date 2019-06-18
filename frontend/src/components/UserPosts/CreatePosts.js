57461b0383638d0fc6bcfb6d6e67fab180527486
Syn <b05902102@ntu.edu.tw>
2019-06-19 00:17:18 +0800

    Update CreatePosts.js

--- frontend/src/components/UserPosts/CreatePosts.js
+++ frontend/src/components/UserPosts/CreatePosts.js
@@ -16,33 +16,12 @@ export default class CreatePosts extends Component {
             text: '',
             location: '',
             photo: [],
-            rate: 2.5
+            rate: 2.5,
+            redirect: false
         };
         this.socket = io.connect('http://localhost:3001');
-
-        this.setName = this.setName.bind(this);
-        this.setText = this.setText.bind(this);
-        this.setLocation = this.setLocation.bind(this);
         this.uploadFigure = this.uploadFigure.bind(this);
-        this.setRate = this.setRate.bind(this);
         this.onSubmitHandler = this.onSubmitHandler.bind(this);
-    }
-
-    setName(e) {
-        this.setState({ name: e.target.value});
-    }
-
-    setText(e) {
-        this.setState({ text: e.target.value});
-    }
-
-    setLocation(e) {
-        this.setState({ location: e.target.value});
-    }
-
-    setRate(e) {
-        this.setState({ rate: e.target.value });
-        console.log("the rate is " + this.state.rate);
     }
 
     uploadFigure = (e) => {
@@ -78,10 +57,8 @@ export default class CreatePosts extends Component {
         // reader.readAsDataURL(file);
     }
 
-    onSubmitHandler(e) {
+    onSubmitHandler = async(e) => {
         e.preventDefault();
-        console.log(Cookie.get('user'));
-        // console.log("The date is " + this.state.y + " " + this.state.m + " " + this.state.d)
         if(!this.state.name) return;
         let newpost = {
             author: this.state.author,
@@ -95,97 +72,98 @@ export default class CreatePosts extends Component {
             rate: this.state.rate
         };
         console.log(newpost);
-        this.setState = {
-            // author: "",
-            name: "",
-            y: "",
-            m: "",
-            d: "",
-            text: "",
-            location: "",
-            photo: [],
-            rate: 2.5
-        }
         this.socket.emit('createPost', newpost);
+        this.setState = {
+            author: Cookie.get('user'),
+            name: '',
+            y: today.getFullYear(),
+            m: today.getMonth()+1,
+            d: today.getDate(),
+            text: '',
+            location: '',
+            photo: [],
+            rate: 2.5,
+            redirect: !this.state.redirect
+        }
+
+        console.log(this.state.name);
     }
 
     render() {
         return (
-        <div className="container" style={{margin: '2rem auto', height: '600px', width: '300px'}}>
-            <form onSubmit={this.onSubmitHandler} >
-                <Form.Group as={Row} controlId="formName">
-                    <label>Name</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter user's name"
-                        value={this.state.name}
-                        onChange={this.setName}
-                    />
-                    <Form.Text className="text-muted">
-                        Your name will be publicly shared.
-                    </Form.Text>
-                </Form.Group>
+            <div style={{height: '600px', width: '300px'}}>
+                <form onSubmit={this.onSubmitHandler}  style={{margin:'auto 1em'}}>
+                    <Form.Group as={Row} controlId="formName">
+                        <label>Name</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter user's name"
+                            value={this.state.name}
+                            onChange={e => this.setState({ name: e.target.value})}
+                        />
+                        <Form.Text className="text-muted">
+                            Your name will be publicly shared.
+                        </Form.Text>
+                    </Form.Group>
 
-                <Form.Group as={Row} controlId="formText">
-                    <label>Description</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={this.state.text}
-                        onChange={this.setText}
-                    />
-                </Form.Group>
+                    <Form.Group as={Row} controlId="formText">
+                        <label>Description</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.text}
+                            onChange={e => this.setState({ text: e.target.value})}
+                        />
+                    </Form.Group>
 
-                <Form.Group as={Row} controlId="formLocation">
-                    <label>Location</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={this.state.location}
-                        onChange={this.setLocation}
-                    />
-                </Form.Group>
+                    <Form.Group as={Row} controlId="formLocation">
+                        <label>Location</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.location}
+                            onChange={e => this.setState({ location: e.target.value})}
+                        />
+                    </Form.Group>
 
-                <Form.Group as={Row} controlId="formFigure">
-                    <Form.Label>Image</Form.Label>
-                    <input 
+                    <Form.Group as={Row} controlId="formFigure">
+                        <Form.Label>Image</Form.Label>
+                        <input 
+                            name="file[]"
+                            id='figure'
+                            type="file"
+                            className='form-control'
+                            multiple onChange={this.uploadFigure}
+                        />
+                    </Form.Group>
 
-                        name="file[]"
-                        id='figure'
-                        type="file"
-                        className='form-control'
-                        multiple onChange={this.uploadFigure}
-                    />
-                </Form.Group>
+                    {this.state.previewURL !== [] && 
+                    this.state.photo.map((url, id) => {
+                        return <img key={id}  style={{height: '16.66rem', width: '16.66rem', marginBottom: '1rem', marginLeft: '-0.5rem'}} src={url} alt="preview" /> })
+                    }
 
-                {this.state.previewURL !== [] && 
-                this.state.photo.map((url, id) => {
-                    return <img key={id}  style={{height: '16.66rem', width: '16.66rem', marginBottom: '1rem', marginLeft: '-0.5rem'}} src={url} alt="preview" /> })
-               }
-
-                <Form.Group as={Row} controlId="formRate">
-                    <input
-                        type="range" 
-                        className="custom-range"
-                        step={0.1}
-                        min ={0}
-                        max={5}
-                        ticks={[1,2,3,4,5]}
-                        value={this.state.rate}
-                        onChange={this.setRate}
-                    />
-                </Form.Group>
-              
-                <Form.Group as={Row} controlId="formBasicChecbox">
-                    <Form.Check type="checkbox" label="Agree" />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>            
-            </form>
-        </div>
-        );
+                    <Form.Group as={Row} controlId="formRate">
+                        <input
+                            type="range" 
+                            className="custom-range"
+                            step={0.1}
+                            min ={0}
+                            max={5}
+                            ticks={[1,2,3,4,5]}
+                            value={this.state.rate}
+                            onChange={e => this.setState({ rate: e.target.value})}
+                        />
+                    </Form.Group>
+                
+                    <Form.Group as={Row} controlId="formBasicChecbox">
+                        <Form.Check type="checkbox" label="Agree" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" style={{marginBottom: '1em'}}>
+                        Submit
+                    </Button>       
+                </form>
+            </div>
+            );
     }
 }
