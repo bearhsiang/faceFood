0922b51b990242588ff1ca6b621d193ca01fc83f
Syn <b05902102@ntu.edu.tw>
2019-06-25 00:01:53 +0800

    Update App.js

--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import webSocket from 'socket.io-client'
-
-// import BottomNav from '../../components/BottomNav';
 import TopNav from '../../components/TopNav';
-// import BotBtn from '../../components/BotBtn';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
-// import MenuList from '../../components/Menu/todos-list';
-// import CreateList from '../../components/Menu/create-todo';
-// import EditList from '../../components/Menu/edit-todo';
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import Auth from '../../components/Home/Auth';
 import UsersList from '../../components/User/UsersList';
 import Profile from '../../components/Home/Profile';
 import Want from '../../components/Home/Want';
-import CreatePost from '../../components/UserPosts/CreatePosts';
 import EachPost from '../../components/UserPosts/EachPost';
 
 class App extends Component {
@@ -32,10 +25,9 @@ class App extends Component {
             {/* <Route path="/" exact component={FoodSearch} /> */}
             <Route path="/" exact component={UsersList}/>
             <Route path='/login' component={Auth}/>
-            <Route path='/users/:id' exact component={Profile}/>
-            <Route path='/post/:id' exact component={Profile}/>
+            <Route path='/users/:id' component={Profile}/>
             <Route path="/:id/wanted" component={Want}/>
-            <Route path='/post/:id' exact component={EachPost}/>
+            <Route path='/post/:id' component={EachPost}/>
             <Redirect to='/' />
           </Switch>
         </Router>
